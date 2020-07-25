9d2fbe719db67f0ecacfaad597abfcfb2611aeff
Allistair Vilakazi <allistair.vilakazi@gmail.com>
2020-07-25 15:52:19 +0200

    feat: refactor to paseto tokens

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,6 +1,7 @@
 import * as bcrypt from 'bcrypt';
 import { Response } from 'express';
 import * as jwt from 'jsonwebtoken';
+import Paseto from 'paseto.js';
 import * as QRCode from 'qrcode';
 import * as speakeasy from 'speakeasy';
 import UserWithThatEmailAlreadyExistsException from '../exceptions/UserWithThatEmailAlreadyExistsException';
@@ -14,9 +15,7 @@ class AuthenticationService {
   public user = userModel;
 
   public async register(userData: CreateUserDto) {
-    if (
-      await this.user.findOne({ email: userData.email })
-    ) {
+    if (await this.user.findOne({ email: userData.email })) {
       throw new UserWithThatEmailAlreadyExistsException(userData.email);
     }
     const hashedPassword = await bcrypt.hash(userData.password, 10);
@@ -33,18 +32,23 @@ class AuthenticationService {
     };
   }
   public getTwoFactorAuthenticationCode() {
-
-    console.log('process.env.TWO_FACTOR_AUTHENTICATION_APP_NAME,', process.env.TWO_FACTOR_AUTHENTICATION_APP_NAME);
+    console.log(
+      'process.env.TWO_FACTOR_AUTHENTICATION_APP_NAME,',
+      process.env.TWO_FACTOR_AUTHENTICATION_APP_NAME,
+    );
 
     const secretCode = speakeasy.generateSecret({
       name: process.env.TWO_FACTOR_AUTHENTICATION_APP_NAME,
     });
     return {
-      otpauthUrl : secretCode.otpauth_url,
+      otpauthUrl: secretCode.otpauth_url,
       base32: secretCode.base32,
     };
   }
-  public verifyTwoFactorAuthenticationCode(twoFactorAuthenticationCode: string, user: User) {
+  public verifyTwoFactorAuthenticationCode(
+    twoFactorAuthenticationCode: string,
+    user: User,
+  ) {
     return speakeasy.totp.verify({
       secret: user.twoFactorAuthenticationCode,
       encoding: 'base32',
@@ -57,16 +61,24 @@ class AuthenticationService {
   public createCookie(tokenData: TokenData) {
     return `Authorization=${tokenData.token}; HttpOnly; Max-Age=${tokenData.expiresIn}`;
   }
-  public createToken(user: User, isSecondFactorAuthenticated = false): TokenData {
+  public async createToken(
+    user: User,
+    isSecondFactorAuthenticated = false,
+  ): Promise<TokenData> {
     const expiresIn = 60 * 60; // an hour
-    const secret = process.env.JWT_SECRET;
     const dataStoredInToken: DataStoredInToken = {
       isSecondFactorAuthenticated,
       _id: user._id,
     };
+
+    const encoder = new Paseto.V2();
+    const symmetricKey = await encoder.symmetric();
+
+    const token = await encoder.encrypt(dataStoredInToken, symmetricKey);
+
     return {
       expiresIn,
-      token: jwt.sign(dataStoredInToken, secret, { expiresIn }),
+      token,
     };
   }
 }
